a41e6e15182bac9a5ec02e6cf1b19cb3d0c5dcb2
Joeybakps <joeybakps@gmail.com>
2021-06-25 08:07:09 +0000

    2 files modified

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
-import React from "react"
+import React from "react";
+import './style';
 
-function App(){
-  return <div>Hello Joel</div>
-}
+
+const App = ()=>(
+<div>
+  <h1>
+    My Todos
+  </h1>
+  <div class="card">
+    <h2 className="action">Title</h2>
+    <div>
+      <button className="btn">Delete</button>
+    </div>
+  </div>
+</div>
+) 
 
 export default App
