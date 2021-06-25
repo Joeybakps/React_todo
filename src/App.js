09995d0734b8ced24112e3d90bf3fe98a2ecf261
Joeybakps <joeybakps@gmail.com>
2021-06-25 15:51:49 +0000

    todo_component_created

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React from "react";
 import './style';
-
+import './components/Todo';
 
 const App = ()=>(
 <div>
   <h1>
     My Todos
   </h1>
-  <div class="card">
-    <h2 className="action">Title</h2>
-    <div>
-      <button className="btn">Delete</button>
-    </div>
-  </div>
+  <Todo text="learn react"/>
+  <Todo text="Master react"/>
+  <Todo text="Explore the fullstack"/>
+
 </div>
 ) 
 
---
+++ src/components/Todo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Todo(props) {
+  return (
+    <div class="card">
+    <h2 className="action">Title</h2>
+    <div>
+      <button className="btn">Delete</button>
+    </div>
+  </div>
+  )
+}
+
+
+
+export default Todo;
